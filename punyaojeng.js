a29dcfa326e7cf0638fae79114a2b3a70e557bce
dimazez26-Ds <dimazez26@gmail.com>
2026-04-01 19:48:42 +0700

    Update punyaojeng.js

--- punyaojeng.js
+++ punyaojeng.js
@@ -1,5 +1,8 @@
-const U = "https://dimazez.kesug.com/finalojeng.user.js";
-const K = "my_js";
+// Link Gist Raw milik Suhana
+const U = "https://gist.githubusercontent.com/dimazez26-Ds/6834b18504ba00faec76083ee302a8c5/raw/f9a98c3de519e3faffeaced7f349a3825289b235/finalojeng.user.js";
+
+// Ganti Key penyimpanan (K) agar QuanX mendownload ulang file yang baru
+const K = "ojeng_final_v1"; 
 
 let c = $prefs.valueForKey(K);
 let b = $response.body;
@@ -7,25 +10,33 @@ let h = $response.headers;
 
 const f = d => {
     if (!b || !h) return $done({});
+    
+    // Hapus proteksi keamanan website target agar script bisa masuk (Inject)
     Object.keys(h).forEach(k => {
         if (/content-security-policy|x-frame-options/i.test(k)) delete h[k];
     });
+    
     h['Cache-Control'] = 'no-cache';
-    // Identik dengan Surge: replace <head> dengan <head><script>content</script>
+    
+    // Inject script SET KB BY OJENG ke dalam tag <head> website
     $done({ body: b.replace(/<head>/i, `<head><script>${d}</script>`), headers: h });
 };
 
 if (c) {
+    // Jika script sudah ada di memori, langsung jalankan
     f(c);
 } else {
+    // Jika belum ada (pertama kali), download dari GitHub Gist
     $task.fetch({ url: U }).then(response => {
         if (response.body) {
             $prefs.setValueForKey(response.body, K);
             f(response.body);
+            console.log("SET KB BY OJENG: Script berhasil di-download dan disimpan.");
         } else {
             $done({});
         }
     }, reason => {
+        console.log("SET KB BY OJENG: Gagal download dari GitHub.");
         $done({});
     });
 }
