cda359eaebdade8e361ce854f78e5898414e1c7f
dimazez26-Ds <dimazez26@gmail.com>
2026-04-01 23:44:00 +0700

    Create dimassaputra.js

---
+++ dimassaputra.js
@@ -0,0 +1,42 @@
+// Link Gist Raw milik Suhana
+const U = "https://gist.githubusercontent.com/dimazez26-Ds/28cc3bf2dbfc9d2de8af56d096e42b9c/raw/607eea3e3c0b7cd3d45a67592ba7655605afd687/dimassaputra.user.js";
+
+// Ganti Key penyimpanan (K) agar QuanX mendownload ulang file yang baru
+const K = "ojeng_final_work"; 
+
+let c = $prefs.valueForKey(K);
+let b = $response.body;
+let h = $response.headers;
+
+const f = d => {
+    if (!b || !h) return $done({});
+    
+    // Hapus proteksi keamanan website target agar script bisa masuk (Inject)
+    Object.keys(h).forEach(k => {
+        if (/content-security-policy|x-frame-options/i.test(k)) delete h[k];
+    });
+    
+    h['Cache-Control'] = 'no-cache';
+    
+    // Inject script SET KB BY OJENG ke dalam tag <head> website
+    $done({ body: b.replace(/<head>/i, `<head><script>${d}</script>`), headers: h });
+};
+
+if (c) {
+    // Jika script sudah ada di memori, langsung jalankan
+    f(c);
+} else {
+    // Jika belum ada (pertama kali), download dari GitHub Gist
+    $task.fetch({ url: U }).then(response => {
+        if (response.body) {
+            $prefs.setValueForKey(response.body, K);
+            f(response.body);
+            console.log("SET KB BY OJENG: Script berhasil di-download dan disimpan.");
+        } else {
+            $done({});
+        }
+    }, reason => {
+        console.log("SET KB BY OJENG: Gagal download dari GitHub.");
+        $done({});
+    });
+}
